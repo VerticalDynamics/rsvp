c9179183daba42ef8e4654a894a9bedb8bbe876b
broneks <bronek@fastmail.fm>
2016-02-14 16:53:40 -0500

    menu improvements

--- partials/menu.php
+++ partials/menu.php
@@ -4,16 +4,17 @@
   <div class="menu-line">&nbsp;</div>
   <div class="menu-line">&nbsp;</div>
 </button>
+
 <nav id="main-nav">
   <button type="button" id="main-nav-close" class="visible-mobile" title="Close menu">&#10005;</button>
   <div class="main-nav-title visible-mobile">Menu</div>
-  <ul>
-    <li><a href="welcome.php">HOME</a></li>
-    <li><a href="details.php">DETAILS</a></li>
-    <li><a href="out_of_town.php">OUT OF TOWN</a></li>
-    <li><a href="rsvp_start.php">RSVP</a></li>
-    <li><a href="registry.php">REGISTRY</a></li>
-    <li><a href="photos.php">PHOTOS</a></li>
+  <ul id="main-nav-links">
+    <li data-page="welcome"><a href="welcome.php">HOME</a></li>
+    <li data-page="details"><a href="details.php">DETAILS</a></li>
+    <li data-page="out-of-town"><a href="out_of_town.php">OUT OF TOWN</a></li>
+    <li data-page="rsvp-start rsvp-confirm rsvp-complete"><a href="rsvp_start.php">RSVP</a></li>
+    <li data-page="registry"><a href="registry.php">REGISTRY</a></li>
+    <li data-page="photos"><a href="photos.php">PHOTOS</a></li>
     <li><a href="logout.php">LOG OUT</a></li>
   </ul>
 </nav>
