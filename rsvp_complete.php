e1fd6efeda0f24aeb719d8cb2c4e9d94af078dbe
Main <Main>
2016-02-13 22:53:54 -0500

    rsvp complete page: code clarity, in-line php improvements

--- rsvp_complete.php
+++ rsvp_complete.php
@@ -18,49 +18,47 @@ require_once 'util/db.php';
   	$guestid = $_SESSION['guestid'];
   	$db = new Database();
   	$conn = $db->openDB();
-  	if ($isconfirmed == 0)
+  	if ($isconfirmed == 0 && $guestid != null)
   	{
-  		if ($guestid != null)
-  		{
-  			$query =
-  			"set @groupname = (select groupname from guest where guestid = :guestid limit 1);
-  			update guest
-  			set guest.isconfirmed = 1
-  			where guest.groupname = @groupname;";
-  			$stmt = $conn->prepare($query);
-  			$stmt->bindParam(':guestid', $guestid);
-  			$stmt->execute();
-  			$_SESSION['isconfirmed'] = 1;
-  			$db->closeDB();
-  		}
-  	}
-  	else
-  	{
-  		$query = "select * from guest where groupname = (select groupname from guest where guestid = :guestid limit 1)";
-  		$stmt = $conn->prepare($query);
-  		$stmt->bindParam(':guestid', $guestid);
-  		$stmt->execute();
-
-  		echo "The following invitees have successfully RSVP'ed: <br><br>";
-
-  		while ($row = $stmt->fetch())
-  		{
-  			echo $row['guestname'] . " ";
-  			$not = ($row['isattending'] == "n" ? "not" : "");
-  			echo " is " . $not . " attending";
-  			if ($row['isplusoneattending'] == 'y')
-  				echo " (and will be bringing a guest).";
-  			else
-  				echo ".";
-  			echo "<br>";
-  		}
-  		echo "<br>";
-  		$db->closeDB();
+		$query = 
+		"set @groupname = (select groupname from guest where guestid = :guestid limit 1);
+		update guest
+		set guest.isconfirmed = 1
+		where guest.groupname = @groupname;";
+		$stmt = $conn->prepare($query);
+		$stmt->bindParam(':guestid', $guestid);
+		$stmt->execute();
+		$_SESSION['isconfirmed'] = 1;
+		$db->closeDB();
   	}
   	?>
+	The following invitees have successfully RSVP'ed:
+	<br>
+	<br>
+	<?php
+	$query = "select * from guest where groupname = (select groupname from guest where guestid = :guestid limit 1)";
+	$stmt = $conn->prepare($query);
+	$stmt->bindParam(':guestid', $guestid);
+	$stmt->execute();
 
-  	<p>If you need to change your selection, please email nkoutros@googlemail.com</p>
-  	<br>
+	$attendance_status_message;
+	while ($row = $stmt->fetch())
+	{
+		$attendance_status_message = 
+			$row['guestname'] . " is" . 
+			($row['isattending'] == "n" ? " not" : "") . " attending" . 
+			($row['isplusoneattending'] == 'y' ? " (and will be bringing a guest)." : ".");
+	?>
+	<?php echo $attendance_status_message ?><br>
+	<?php
+	}
+	$db->closeDB();
+  	?>
+	<br>
+  	Your RSVP has been received and is now locked.<br>
+	<br>
+	If you need to change your selection, please email nkoutros@googlemail.com for assistance.<br>
+	<br>
   	<form action="welcome.php" method="post">
   		<input type="submit" value="You're Done, Thanks! Return to Main Page">
   	</form>
