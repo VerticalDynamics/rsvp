8499c965e50569e90224c6ed9d34ee31f1a9c222
VerticalDynamics <skildreg@gmail.com>
2016-03-01 15:23:41 -0500

    added out of town details

--- out_of_town.php
+++ out_of_town.php
@@ -10,8 +10,37 @@
 
   <div class="splash-header">
     <div class="container">
-      <h2>Not familiar with Windsor?</h2>
+      <h2>Not familiar with <strong>Windsor</strong>?</h2>
       <h3>Here's how to get there and where to stay</h3>
+      <div class="row">
+        <div class="six columns details-column">
+          <h3 class="details-title"><strong>Transportation</strong></h3>
+          <h4>By Air</h4> 
+          <ul>
+            <li><a style="color:white" href="https://www.google.ca/flights/#search;f=YOW;t=YQG;d=2016-08-26;r=2016-08-31" target="_blank">Windsor International Airport (YQG)</a></li>
+            <li><a style="color:white" href="https://www.google.ca/flights/#search;f=YOW;t=DTW;d=2016-08-26;r=2016-08-31" target="_blank">Detroit Metro Airport (DTW)</a></li>
+          </ul>
+          <h4>By Train & Bus</h4> 
+          <ul>
+            <li><a style="color:white" href="http://www.viarail.ca/en/explore-our-destinations/stations/ontario/windsor" target="_blank">Windsor Train Station</a></li>
+            <li><a style="color:white" href="http://www.citywindsor.ca/residents/transitwindsor/Ride-Guide/Pages/Transit-Terminals.aspx" target="_blank">Windsor Bus Terminal</a></li>
+          </ul>
+          <h4>By Taxi</h4> 
+          <ul>
+            <li><a style="color:white" href="https://www.uber.com" target="_blank">Uber</a> (10% off code: nicholask1006ue)</li>
+            <li><a style="color:white" href="http://www.vetscab.com" target="_blank">Vets Cab</a> (519-265-2621)</li>
+          </ul>
+        </div>
+        <div class="six columns details-column">
+          <h3 class="details-title"><strong>Accomodation</strong></h3>
+          <ul>
+            <li>Recommended Hotel: <a style="color:white" href="http://www.travelodge.ca/property/travelodge-hotel-windsor-downtown" target="_blank">Travelodge Downtown Windsor</a></li>
+          </ul>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d94444.98414336013!2d-83.08711038645778!3d42.291206840009146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883b2d1f659a1ac9%3A0x704b162d304d8119!2sTravelodge+Hotel+Downtown+Windsor!5e0!3m2!1sen!2sca!4v1456863296788" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+
     </div>
   </div>
 
